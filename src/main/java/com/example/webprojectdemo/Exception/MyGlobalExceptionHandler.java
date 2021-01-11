0c30535fc17455a0c0fd3dfcc2d335811fe2c717
zhangjie <550048625@qq.com>
2021-01-11 11:27:08 +0800

    1-11

---
+++ src/main/java/com/example/webprojectdemo/Exception/MyGlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.webprojectdemo.Exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class MyGlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ModelAndView customException(Exception e) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", e.getMessage());
+        mv.setViewName("myerror");
+        return mv;
+    }
+
+
+    @ModelAttribute(name = "md")
+    public Map<String,Object> mydata() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("age", 99);
+        map.put("gender", "男");
+        return map;
+    }
+}
--- src/main/java/com/example/webprojectdemo/controller/indexController.java
+++ src/main/java/com/example/webprojectdemo/controller/indexController.java
@@ -1,13 +1,38 @@
 package com.example.webprojectdemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
+//@RestController
 @Controller
+@Slf4j
 public class indexController {
 
     @RequestMapping("/index")
-    public String index(){
-        return "index";
+    public Object index(@RequestParam("name") String name , String userId, Model model){
+        Map<String, Object> map2 = model.asMap();
+        System.out.println(map2);
+        log.info("name = {}; userId = {}",name ,userId);
+        HashMap map = new HashMap();
+        map.put("name","zhangjie");
+        map.put("usrId","234");
+        return map;
     }
+
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        Map<String, Object> map = model.asMap();
+        System.out.println(map);
+        int i = 1 / 0;
+        return "hello controller advice";
+    }
+
 }
